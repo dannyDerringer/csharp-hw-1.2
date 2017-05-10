9ab074950972e42f898a308b6d0a81c92bd6868c
Danny Derringer <danny.derringer@gmail.com>
2017-05-10 15:31:33 -0400

    Multiplication table function
    2. Multiplication tables are fundamental to mathematics. Write a
    function that accepts a single integer, int n for example, and prints
    out a n x n multiplication table. For example, if I call your function
    with the parameter 5, then I expect to see a table printed that looks
    like the following.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2{
+    class Program{
+        static void Main(string[] args){
+            multiplicationtable(9);
+        }
+
+        static void multiplicationtable(int n){
+            int r = 1;
+
+            for (int j = 0; j < n; j++){
+
+                for (int i = 0; i < n; i++){
+                    int t = r * (i + 1);
+                    
+                    if (t < 10){
+                        Console.Write(" " + t + "  ");
+                    }
+
+                    else{
+                        Console.Write(t + "  ");
+                    }
+                }
+
+            Console.Write("\n");
+            r++;
+            }
+        }
+    }
+}
+
